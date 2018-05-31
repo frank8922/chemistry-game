10b6b3bc1dc9e1779f29fbd22d9ea75455c74a43
Jason Balladares <tvvizzlemac@JasonsMacBookPro.hsd1.fl.comcast.net>
2018-05-31 00:00:50 -0400

    Fixed Scripts,aspect ratio, and better demo

--- Drag-and-drop-demo/Assets/Scripts/DraggingObjectsAlkali.cs
+++ Drag-and-drop-demo/Assets/Scripts/DraggingObjectsAlkali.cs
@@ -3,23 +3,30 @@ using System.Collections;
 
 public class DraggingObjectsAlkali : MonoBehaviour
 {
-	Rigidbody2D rb;
+	private Rigidbody2D rb;
 
-
-
+	//use to as init function
 	void Start()
 	{
-
+		Debug.Log("Start() for " + this.gameObject + " was called");
 		// Store reference to attached Rigidbody
 		rb = GetComponent<Rigidbody2D>();
-		Physics2D.IgnoreLayerCollision(8,9);
 
+		//ignore 2Dphysics on certain layers when they collide
+		Physics2D.IgnoreLayerCollision(10,10);
+		//Physics2D.IgnoreLayerCollision(8,9);
+		//Physics2D.IgnoreLayerCollision(8,8);
+		//Physics2D.IgnoreLayerCollision(9,9);
+		//Physics2D.IgnoreLayerCollision(9,8);
 	}
 
 
-
+	//when the mouse is dragging this function is called 
+	//can be thought of as a touch drag
+	//TODO: Use touch instead of Mouse
 	void OnMouseDrag()
 	{
+		Debug.Log("OnMouseDrag() for " + this.gameObject + " was called");
 		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 
 		// Move by Rigidbody rather than transform directly
@@ -29,24 +36,31 @@ public class DraggingObjectsAlkali : MonoBehaviour
 	}
 
 
-
-
-
+	//can be thought of as when user stops touching
+	//TODO: Use touch instead of Mouse
 	void OnMouseExit()
 	{
+		Debug.Log("OnMouseExit() for " + this.gameObject + " was called");
 		gameObject.transform.localScale = new Vector3(1, 1, 1);
 	}
+
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log("OnTriggerEnter2D was triggered");
-		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-		if (other.gameObject.CompareTag("Alkali"))
+		Debug.Log("The OnTriggerEnter2D() for "  + other.gameObject.name + " was called");
+
+		//Check the provided Collider2D parameter other to see if it is tagged "foo", if it is...
+		if (other.gameObject.CompareTag("Alkaline"))
 		{
 			Destroy(this.gameObject);
-			Debug.Log("gameobject destroyed");
+			Debug.Log(this.gameObject.name + " destroyed by");
 
+		}
+		else if (other.gameObject.CompareTag("BottomBoxCollider")) 
+		{ 
+            Destroy(this.gameObject);
+			Debug.Log(this.gameObject.name + " destroyed by bottom box collider");
 		}
 
 		else
@@ -54,7 +68,6 @@ public class DraggingObjectsAlkali : MonoBehaviour
 
 			//this.gameObject.transform.position = new Vector3(0,0);
 			Debug.Log("else statement is ran");
-
 
 		}
 
