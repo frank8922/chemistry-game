03e18d7396a06723b4bdaae109fe9953807f521e
David Freer <david.freer@gmail.com>
2018-06-15 23:05:13 -0400

    Message for incorrect login.

--- Drag and Drop Demo/Assets/Scripts/FirebaseScript.cs
+++ Drag and Drop Demo/Assets/Scripts/FirebaseScript.cs
@@ -12,11 +12,30 @@ public class FirebaseScript : MonoBehaviour {
 	public InputField EmailAddress, Password;
 
 	public void LoginButtonPressed(){
-		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text, Password.text).ContinueWith((obj) =>
-		{
-			SceneManager.LoadSceneAsync("Level1");
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text, Password.text).ContinueWith((task) =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            else if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                EditorUtility.DisplayDialog("Incorrect Login", "Password or email is incorrect.", "Fix it.");
+                return;
+            }
+            else
+            {
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+                SceneManager.LoadSceneAsync("Level1");
+            }
 		});
-	}
+
+        
+    }
 
 	public void LoginAnonymousButtonPressed(){
 		FirebaseAuth.DefaultInstance.SignInAnonymouslyAsync().ContinueWith((obj)=>
