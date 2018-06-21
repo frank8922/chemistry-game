c9144352d20318ac1e13dae75ebf7a468f9247eb
frank8922 <frank8922@gmail.com>
2018-06-21 09:50:31 -0400

    added login dialog  functionality
    -Login button now brings up the login dialog box

---
+++ Drag and Drop Demo/Assets/Scripts/OpenDialogBoxScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDialogBoxScript : MonoBehaviour {
+	
+	public Animator animator;
+	public GameObject loginDialogObject;
+	
+	public void openDialog(){
+
+		loginDialogObject.SetActive(true);
+		animator.SetTrigger("OpenLoginDialog");
+	}
+
+	public void closeDialog(){
+		animator.SetTrigger("CloseLoginDialog");
+		
+	}
+
+}
