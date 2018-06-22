639b7f80959ac9cee32cda5d5ba674b8ea112c49
frank8922 <frank8922@gmail.com>
2018-06-22 01:25:52 -0400

    Added Icons to dialog boxes & adjusted animation time
    -Added Email & Password Icons
    -Adjusted animation times
    -Adjusted anchor points
    -Changed close button on dialogs
    -Created prefabs for dialogs and buttons

---
+++ Drag and Drop Demo/Assets/Scripts/GoogleSignOnController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoogleSignOnController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+  var dependencyStatus = task.Result;
+  if (dependencyStatus == Firebase.DependencyStatus.Available) {
+    // Set a flag here indiciating that Firebase is ready to use by your
+    // application.
+  } else {
+    UnityEngine.Debug.LogError(System.String.Format(
+      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+    // Firebase Unity SDK is not safe to use here.
+  }
+});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
